5e12fcccac67b234ef24a264a7d6c3a07522bc12
Rheeneil Cabahug <rheeneil.cabahug@googlemail.com>
2026-01-22 17:41:49 +0000

    fixed commands and faker issues

--- cypress/e2e/QAPractice.cy.js
+++ cypress/e2e/QAPractice.cy.js
@@ -1,6 +1,4 @@
-import { before } from "lodash";
 import { generateData } from "../support/dataGenerator";
-import { da, it } from "@faker-js/faker";
 
 /// <reference types="cypress" />
 
@@ -12,101 +10,98 @@ describe("Forms Pages: Login Scenarios", () => {
     cy.waitForElement(".display-4", 5000, "Welcome!");
   });
 
-  it("Opens Forms Page: Login - Success", () => {
-    cy.clickButton("#forms");
-    cy.waitForElement("#login", 5000, "Login");
-    cy.clickButton("#login");
-    cy.waitForURL("/auth_ecommerce.html");
-    cy.waitForElement("#loginSection");
-    cy.login(
-      "#email",
-      "admin@admin.com",
-      "#password",
-      "admin123",
-      "#submitLoginBtn",
-    );
-    cy.waitForElement("#logout", 5000, "Log Out");
+  context("Login Scenarios", () => {
+    it("Login - Success", () => {
+      cy.clickButton("#forms");
+      cy.waitForElement("#login", 5000, "Login");
+      cy.clickButton("#login");
+      cy.waitForURL("/auth_ecommerce.html");
+      cy.waitForElement("#loginSection");
+      cy.login(
+        "#email",
+        "admin@admin.com",
+        "#password",
+        "admin123",
+        "#submitLoginBtn",
+      );
+      cy.waitForElement("#logout", 5000, "Log Out");
+    });
+
+    it("Login - Invalid Password", () => {
+      cy.clickButton("#forms");
+      cy.waitForElement("#login", 5000, "Login");
+      cy.clickButton("#login");
+      cy.waitForURL("/auth_ecommerce.html");
+      cy.waitForElement("#loginSection");
+      cy.login(
+        "#email",
+        "admin@admin.com",
+        "#password",
+        "invalidpassword",
+        "#submitLoginBtn",
+      );
+      cy.waitForElement(
+        "#message",
+        5000,
+        "Bad credentials! Please try again! Make sure that you've registered.",
+      );
+    });
+
+    it("Login - Invalid Email", () => {
+      cy.clickButton("#forms");
+      cy.waitForElement("#login", 5000, "Login");
+      cy.clickButton("#login");
+      cy.waitForURL("/auth_ecommerce.html");
+      cy.waitForElement("#loginSection");
+      cy.login(
+        "#email",
+        "invalidemail@example.com",
+        "#password",
+        "admin123",
+        "#submitLoginBtn",
+      );
+      cy.waitForElement(
+        "#message",
+        5000,
+        "Bad credentials! Please try again! Make sure that you've registered.",
+      );
+    });
+
+    it("Login - Empty Fields", () => {
+      cy.clickButton("#forms");
+      cy.waitForElement("#login", 5000, "Login");
+      cy.clickButton("#login");
+      cy.waitForURL("/auth_ecommerce.html");
+      cy.waitForElement("#loginSection");
+      cy.clickButton("#submitLoginBtn");
+      cy.waitForElement(
+        "#message",
+        5000,
+        "Bad credentials! Please try again! Make sure that you've registered.",
+      );
+    });
   });
 
-  it("Opens Forms Page: Login - Invalid Password", () => {
-    cy.clickButton("#forms");
-    cy.waitForElement("#login", 5000, "Login");
-    cy.clickButton("#login");
-    cy.waitForURL("/auth_ecommerce.html");
-    cy.waitForElement("#loginSection");
-    cy.login(
-      "#email",
-      "admin@admin.com",
-      "#password",
-      "invalidpassword",
-      "#submitLoginBtn",
-    );
-    cy.waitForElement(
-      "#message",
-      5000,
-      "Bad credentials! Please try again! Make sure that you've registered.",
-    );
-  });
-
-  it("Opens Forms Page: Login - Invalid Email", () => {
-    cy.clickButton("#forms");
-    cy.waitForElement("#login", 5000, "Login");
-    cy.clickButton("#login");
-    cy.waitForURL("/auth_ecommerce.html");
-    cy.waitForElement("#loginSection");
-    cy.login(
-      "#email",
-      "invalidemail@example.com",
-      "#password",
-      "admin123",
-      "#submitLoginBtn",
-    );
-    cy.waitForElement(
-      "#message",
-      5000,
-      "Bad credentials! Please try again! Make sure that you've registered.",
-    );
-  });
-
-  it("Opens Forms Page: Login - Empty Fields", () => {
-    cy.clickButton("#forms");
-    cy.waitForElement("#login", 5000, "Login");
-    cy.clickButton("#login");
-    cy.waitForURL("/auth_ecommerce.html");
-    cy.waitForElement("#loginSection");
-    cy.clickButton("#submitLoginBtn");
-    cy.waitForElement(
-      "#message",
-      5000,
-      "Bad credentials! Please try again! Make sure that you've registered.",
-    );
-  });
-});
-
-describe("Forms Pages: Registration Scenarios", () => {
-  beforeEach(() => {
-    cy.visit("https://www.qa-practice.razvanvancea.ro");
-    cy.waitForElement(".display-4", 5000, "Welcome!");
-  });
-  it("Opens Forms Page: Registration - Success", () => {
-    cy.clickButton("#forms");
-    cy.waitForElement("#register", 5000, "Register");
-    cy.clickButton("#register");
-    cy.waitForURL("/register.html");
-    cy.waitForElement("#registerForm");
-    cy.fillInput("#firstName", data.firstName);
-    cy.fillInput("#lastName", data.lastName);
-    cy.fillInput("#phone", data.phoneNumber);
-    cy.fillInput("#email", data.emailAddress);
-    cy.selectOption("#countries_dropdown_menu", "United Kingdom");
-    cy.fillInput("#emailAddress", data.emailAddress);
-    cy.fillInput("#password", data.password);
-    cy.checkCheckbox("#exampleCheck1");
-    cy.clickButton("#registerBtn");
-    cy.waitForElement(
-      "#message",
-      5000,
-      "The account has been successfully created!",
-    );
+  context("Registration Scenarios", () => {
+    it("Opens Forms Page: Registration - Success", () => {
+      cy.clickButton("#forms");
+      cy.waitForElement("#register", 5000, "Register");
+      cy.clickButton("#register");
+      cy.waitForURL("/register.html");
+      cy.waitForElement("#registerForm");
+      cy.fillInput("#firstName", data.firstName);
+      cy.fillInput("#lastName", data.lastName);
+      cy.fillInput("#phone", data.telephoneNumber);
+      cy.selectOption("#countries_dropdown_menu", "United Kingdom");
+      cy.fillInput("#emailAddress", data.emailAddress);
+      cy.fillInput("#password", data.password);
+      cy.checkCheckbox("#exampleCheck1");
+      cy.clickButton("#registerBtn");
+      cy.waitForElement(
+        "#message",
+        5000,
+        "The account has been successfully created!",
+      );
+    });
   });
 });
--- cypress/support/dataGenerator.js
+++ cypress/support/dataGenerator.js
@@ -1,44 +1,46 @@
-import { faker, fakerEN_GB } from '@faker-js/faker';
-
+import { faker, fakerEN_GB } from "@faker-js/faker";
 
 export function generateData() {
-    const firstName = faker.person.firstName();
-    const lastName = faker.person.lastName();
-    let fullDOB = faker.date.birthdate({min: 18, max: 75, mode: 'age'});
-    fullDOB = String(fullDOB);
-    let date = fullDOB.split("T")[0];
-    let day = date.split("-")[2];
-    let month = date.split("-")[1];
-    let year = date.split("-")[0];
-    const dob = day + '/' + month + '/' + year;
-    const emailAddress = faker.internet.email();
-    const password = "password" + (Math.floor(Math.random() * 2));
-    const address = [fakerEN_GB.location.streetAddress, fakerEN_GB.location.street];
-    const telephoneNumber = faker.phone.number('176########');
-    const randomPin = Math.floor(Math.random() * 9000 + 1000);
-    const postCode = fakerEN_GB.location.zipCode();
-    
-    const data = {
-        'firstName': firstName,
-        'lastName': lastName,
-        'dateOfBirth': dob,
-        'emailAddress': emailAddress,
-        'password': password,
-        'address': address,
-        'telephoneNumber': telephoneNumber,
-        'pin': randomPin,
-        'zipCode': postCode
-    }
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  let fullDOB = faker.date.birthdate({ min: 18, max: 75, mode: "age" });
+  fullDOB = String(fullDOB);
+  let date = fullDOB.split("T")[0];
+  let day = date.split("-")[2];
+  let month = date.split("-")[1];
+  let year = date.split("-")[0];
+  const dob = day + "/" + month + "/" + year;
+  const emailAddress = faker.internet.email();
+  const password = "password" + Math.floor(Math.random() * 2);
+  const address = [
+    fakerEN_GB.location.streetAddress,
+    fakerEN_GB.location.street,
+  ];
+  const telephoneNumber = fakerEN_GB.phone.number("+447#########");
+  const randomPin = Math.floor(Math.random() * 9000 + 1000);
+  const postCode = fakerEN_GB.location.zipCode();
 
-    console.log('First Name: ' + firstName + ' ' + typeof firstName);
-    console.log('Last Name: ' + lastName + ' ' + typeof lastName);
-    console.log('Date of Birth: ' + dob)
-    console.log('Email Address: ' + emailAddress + ' ' + typeof emailAddress);
-    console.log('password: ' + password + ' ' + typeof password);
-    console.log('Address: ' + address)
-    console.log('Telephone: ' + telephoneNumber + ' ' + typeof telephoneNumber);
-    console.log('Random Pin: ' + randomPin + ' ' + typeof randomPin)
-    console.log('Zip Code: ' + postCode + ' ' + typeof zipCode) 
+  const data = {
+    firstName: firstName,
+    lastName: lastName,
+    dateOfBirth: dob,
+    emailAddress: emailAddress,
+    password: password,
+    address: address,
+    telephoneNumber: telephoneNumber,
+    pin: randomPin,
+    zipCode: postCode,
+  };
 
-    return data;
+  console.log("First Name: " + firstName + " " + typeof firstName);
+  console.log("Last Name: " + lastName + " " + typeof lastName);
+  console.log("Date of Birth: " + dob);
+  console.log("Email Address: " + emailAddress + " " + typeof emailAddress);
+  console.log("password: " + password + " " + typeof password);
+  console.log("Address: " + address);
+  console.log("Telephone: " + telephoneNumber + " " + typeof telephoneNumber);
+  console.log("Random Pin: " + randomPin + " " + typeof randomPin);
+  console.log("Zip Code: " + postCode + " " + typeof zipCode);
+
+  return data;
 }
